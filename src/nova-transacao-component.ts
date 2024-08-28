d24df0fe9d9fe54c4608f1ac88e34c65ff3d4d4f
amanciolari <alveslariamancio@gmail.com>
2024-08-28 14:50:53 -0300

    Reorganizando o código

--- src/nova-transacao-component.ts
+++ src/nova-transacao-component.ts
@@ -1,14 +1,3 @@
-let saldo = 3000;
-
-alert("Testando compilação do TS");
-
-const elementoSaldo = document.querySelector(
-  ".saldo-valor .valor"
-) as HTMLElement;
-if (elementoSaldo != null) {
-  elementoSaldo.textContent = saldo.toString();
-}
-
 const elementoFormulario = document.querySelector(
   ".block-nova-transacao form"
 ) as HTMLFormElement;
