cf137757ba658882af29654ea106143eef445f57
amanciolari <alveslariamancio@gmail.com>
2024-08-30 11:35:07 -0300

    Lançando erros

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,31 +1,46 @@
-import { Transacao } from "./Transacao".js;
+import { Transacao } from "./Transacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
 
 let saldo: number = 3000;
 
-const Conta = {
-    getSaldo() {
-        return saldo;
-    },
-
-    getDataAcesso(): Date {
-        return new Date();
-    },
-
-    registrarTransacao(novaTransacao: Transacao): void{
-        if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-            saldo += novaTransacao.valor;
-          } else if (
-            novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA ||
-            novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO
-          ) {
-            saldo -= novaTransacao.valor;
-          } else {
-            alert("Tipo de Transação é inválido!");
-            return;
-          }
-    }
-
+function debitar(valor: number): void {
+  if (valor <= 0) {
+    throw new Error("O valor a ser debitado deve ser maior que zero!");
+  }
+  if (valor > saldo) {
+    throw new Error("Saldo insuficiente!");
+  }
+  saldo -= valor;
 }
+
+function depositar(valor: number): void {
+  if (valor <= 0) {
+    throw new Error("O valor a ser depositado deve ser maior que zero!");
+  }
+  saldo += valor;
+}
+
+const Conta = {
+  getSaldo() {
+    return saldo;
+  },
+
+  getDataAcesso(): Date {
+    return new Date();
+  },
+
+  registrarTransacao(novaTransacao: Transacao): void {
+    if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
+      depositar(novaTransacao.valor);
+    } else if (
+      novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA ||
+      novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO
+    ) {
+      debitar(novaTransacao.valor);
+    } else {
+      throw new Error("Tipo de Transação é inválido!");
+    }
+  },
+};
 
 export default Conta;
